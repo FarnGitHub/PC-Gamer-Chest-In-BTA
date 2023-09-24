9b406aaa47340613aef6b4de894e0de03726a243
Farn <93525542+FarnGitHub@users.noreply.github.com>
2023-09-24 14:00:53 +0700

    Fix Bug And Crash

--- src/main/java/farn/pcGamerChest/Main.java
+++ src/main/java/farn/pcGamerChest/Main.java
@@ -1,6 +1,8 @@
 package farn.pcGamerChest;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.block.BlockChest;
+import net.minecraft.core.util.helper.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +10,7 @@ import java.util.Random;
 
 import net.minecraft.core.world.World;
 import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockChestLegacy;
 import net.minecraft.core.block.entity.TileEntitySign;
 import net.minecraft.core.block.entity.TileEntityChest;
 import net.minecraft.core.item.Item;
@@ -27,45 +30,27 @@ public class Main implements ModInitializer {
 
     public static void initChest(World world) {
         Random gunPowderRand = new Random(19L);
-
-        /*
-        Get chest location
-         */
         int chestX = world.getSpawnPoint().x + 2;
         int chestZ = world.getSpawnPoint().z + 2;
         int chestY = world.findTopSolidBlock(chestX, chestZ);
 
-        /*
-        Double chest
-         */
-        world.setBlockWithNotify(chestX, chestY, chestZ - 1, Block.chestLegacy.id);
+        world.setBlockAndMetadataWithNotify(chestX, chestY, chestZ - 1, Block.chestPlanksOak.id, 6);
         TileEntityChest chest1 = (TileEntityChest)world.getBlockTileEntity(chestX, chestY, chestZ - 1);
-        world.setBlockWithNotify(chestX + 1, chestY, chestZ - 1, Block.chestLegacy.id);
+        world.setBlockAndMetadataWithNotify(chestX + 1, chestY, chestZ - 1, Block.chestPlanksOak.id, 10);
         TileEntityChest chest2 = (TileEntityChest)world.getBlockTileEntity(chestX + 1, chestY, chestZ - 1);
 
-        /*
-        Gun Powder chest ( below double chest )
-         */
-        world.setBlockWithNotify(chestX, chestY - 4, chestZ, Block.chestLegacy.id);
+        world.setBlockWithNotify(chestX, chestY - 4, chestZ, Block.chestPlanksOak.id);
         TileEntityChest chestSecret = (TileEntityChest)world.getBlockTileEntity(chestX, chestY - 4, chestZ);
 
-        /*
-        sign
-         */
         world.setBlockWithNotify(chestX, chestY, chestZ, Block.signPostPlanksOak.id);
-        /*
-        Sign content
-         */
-        ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[0] = "Have some stuff!";
-        ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[1] = "/ PC Gamer";
-        ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[2] = "p.s. Right click";
-        ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[3] = "to open!";
 
-        /*
-        Double chest content
+        if(world.getBlockTileEntity(chestX, chestY, chestZ) != null) {
+            ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[0] = "Have some stuff!";
+            ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[1] = "/ PC Gamer";
+            ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[2] = "p.s. Right click";
+            ((TileEntitySign)world.getBlockTileEntity(chestX, chestY, chestZ)).signText[3] = "to open!";
+        }
 
-        wtf , flint and steel are called fire striker
-         */
         chest1.setInventorySlotContents(7, new ItemStack(Item.toolFirestriker, 1, 50));
         chest1.setInventorySlotContents(3, new ItemStack(Item.toolAxeIron, 1, 122));
         chest1.setInventorySlotContents(10, new ItemStack(Item.toolPickaxeIron, 1, 140));
@@ -81,9 +66,6 @@ public class Main implements ModInitializer {
         chest2.setInventorySlotContents(11, new ItemStack(Item.stick, 2));
         chest2.setInventorySlotContents(26, new ItemStack(Item.toolCompass, 3));
 
-        /*
-        GunPowder chest content
-         */
         for(int i7 = 0; i7 < 7; ++i7) {
             chestSecret.setInventorySlotContents(gunPowderRand.nextInt(27), new ItemStack(Item.sulphur, gunPowderRand.nextInt(10) + 1));
         }
